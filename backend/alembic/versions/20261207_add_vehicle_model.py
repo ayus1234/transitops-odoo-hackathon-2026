be4f9c9a5ffe93dc502c8245741cf4fa807bca4b
Ayush Nathani <ayushnathani1234@gmail.com>
2026-08-08 20:05:08 +0530

    fix(migrations): add initial roles and users migration 20261206_add_roles_and_users.py to fix relation users does not exist in alembic upgrade head

--- backend/alembic/versions/20261207_add_vehicle_model.py
+++ backend/alembic/versions/20261207_add_vehicle_model.py
@@ -11,7 +11,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = '20261207_vehicle'
-down_revision = None
+down_revision = '20261206_users_roles'
 branch_labels = None
 depends_on = None
 
